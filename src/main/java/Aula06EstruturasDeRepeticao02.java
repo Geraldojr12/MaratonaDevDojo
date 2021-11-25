802815003ff51b29fb0c780e006d97019582ccf1
GeraldoJr12 <junior4231@hotmail.com>
2021-11-25 16:23:53 -0300

    Aula06 Exercicio2 de Estruturas de Repeticao

--- src/main/java/Aula06EstruturasDeRepeticao02.java
+++ src/main/java/Aula06EstruturasDeRepeticao02.java
@@ -3,7 +3,7 @@ public class Aula06EstruturasDeRepeticao02 {
         // Imprima todos os numeros pares de 0 a 1000000
 
         for (int i = 0; i < 50; i++) {
-            if (i % 2 ==0 ){
+            if (i % 2 == 0 ){
                 System.out.println(i);
             }
         }
